f672d5009f81e240610bab7f9dd55531c5c74ba4
IngvarJackal <IngvarJackal@users.noreply.github.com>
2026-04-21 03:56:53 +0200

    try to fix nullability inference issues

--- Content.IntegrationTests/Tests/Shuttle/ShipSerializationTest.cs
+++ Content.IntegrationTests/Tests/Shuttle/ShipSerializationTest.cs
@@ -17,6 +17,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Maths;
 using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
+#nullable enable
 namespace Content.IntegrationTests.Tests.Shuttle;
 
 /// <summary>
